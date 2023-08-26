460edd279afce57ec579d008d0b5e6b63de7c1cf
aondrej ryan <aondrejryan11@gmail.com>
2023-08-26 13:18:29 -0700

    woeking on tredning playlist

--- client/src/component/PlaylistTrack.tsx
+++ client/src/component/PlaylistTrack.tsx
@@ -20,16 +20,6 @@ function PlaylistTrack(props: any) {
           const data = await response.json();
           setPlaylistSongs(data.data[0].playlist_contents)
 
-          console.log(data.data[0].playlist_contents)
-          const songDataPromises = playlistSongs.slice(0,5).map(async (song: any) => {
-            const response = await fetch(`https://blockchange-audius-discovery-01.bdnodes.net/v1/tracks/${song.track_id}?app_name=drejapp`, {
-              method: 'GET',
-            });
-            const data = await response.json();
-            return data.data;
-          })
-          const resolvedSongData = await Promise.all(songDataPromises);
-          setPlaylistSongs(resolvedSongData);
           // setArtwork(data.data[0].artwork['150x150'])
           // setPlaylistName(data.data[0].playlist_name)
           // setCreatorName(data.data[0].user.name)
@@ -40,19 +30,38 @@ function PlaylistTrack(props: any) {
     fetchData()
   }, [])
 
-  // React.useEffect(() => {
-  //   const fetchData = 
-  // })
+    React.useEffect(() => {
+      const fetchData = async () => {
+        try {
+            const resolvedSongData = [];
 
-  
-// if(data){
-//   console.log(playlistSongs)
-// }
+            for (const song of playlistSongs) {
+              if(song.track_id !== undefined){
+                  const response = await fetch(`https://blockchange-audius-discovery-01.bdnodes.net/v1/tracks/${song.track_id}?app_name=drejapp`, {
+                  method: 'GET',
+                });
+                
+                const data = await response.json();
+                resolvedSongData.push(data.data);
+              }
+            }
+
+            setPlaylistSongs(resolvedSongData);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+
+      fetchData();
+    }, [])
 
 
+if(data){
+  console.log(playlistSongs)
+}
   return (
     <>
-      {data && data !== null ? (
+      {/* {data && data !== null ? (
         playlistSongs.slice(0,5).map(async( song: any, i:number) => {
             
     
@@ -60,7 +69,7 @@ function PlaylistTrack(props: any) {
             <div>lol</div>
           )
         })
-      ) : null}
+      ) : null} */}
         {/* <div className='playlist-track flex'>
         {false ? <img src={`${artwork}`} alt="playlist cover" className='empty-div bg-red-700'></img> : <span className='empty-div bg-white-700'></span>}
             <div className='flex justify-between playlist-song w-full'>
