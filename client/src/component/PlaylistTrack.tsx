aa56ae2a20b5809e13c443929277339871f5f46a
aondrej ryan <aondrejryan11@gmail.com>
2023-08-26 15:04:21 -0700

    woeking on tredning playlist

--- client/src/component/PlaylistTrack.tsx
+++ client/src/component/PlaylistTrack.tsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom'
 function PlaylistTrack(props: any) {
   const [data,setData] = React.useState<any[]>([])
   const [playlistSongs,setPlaylistSongs] = React.useState<any[]>([])
-  const [artwork, setArtwork] = React.useState<string>('')
-  const [playlistName, setPlaylistName] = React.useState<string>('')
-  const [creatorName, setCreatorName] = React.useState<string>('')
+  const [idk,setIdk] = React.useState<any[]>([])
+  // const [artwork, setArtwork] = React.useState<string>('')
+  // const [playlistName, setPlaylistName] = React.useState<string>('')
+  // const [creatorName, setCreatorName] = React.useState<string>('')
   React.useEffect(() => {
     const fetchData = async () => {
       try {
@@ -20,6 +21,14 @@ function PlaylistTrack(props: any) {
           const data = await response.json();
           setPlaylistSongs(data.data[0].playlist_contents)
 
+          const songsWithAsyncData = await Promise.all(
+            data.data[0].playlist_contents.map(async (song: any) => {
+              const asyncDataResponse = await fetch(`https://blockchange-audius-discovery-01.bdnodes.net/v1/tracks/${song.track_id}??app_name=drejapp`);
+              const asyncData = await asyncDataResponse.json();
+              return { asyncData };
+            })
+          )
+            setIdk(songsWithAsyncData)
           // setArtwork(data.data[0].artwork['150x150'])
           // setPlaylistName(data.data[0].playlist_name)
           // setCreatorName(data.data[0].user.name)
@@ -29,56 +38,43 @@ function PlaylistTrack(props: any) {
     }
     fetchData()
   }, [])
+console.log(idk)
+//   function fetchData(){
+//     playlistSongs.slice(0,5).forEach(async (song: any) => {
+//       const response = await fetch(`https://blockchange-audius-discovery-01.bdnodes.net/v1/tracks/${song.track_id}?app_name=EXAMPLEAPP`, {
+//         method: 'GET',
+//       })
 
-    React.useEffect(() => {
-      const fetchData = async () => {
-        try {
-            const resolvedSongData = [];
+//       let data = await response.json()
 
-            for (const song of playlistSongs) {
-              if(song.track_id !== undefined){
-                  const response = await fetch(`https://blockchange-audius-discovery-01.bdnodes.net/v1/tracks/${song.track_id}?app_name=drejapp`, {
-                  method: 'GET',
-                });
-                
-                const data = await response.json();
-                resolvedSongData.push(data.data);
-              }
-            }
+//       iSuckAtThisShit.push(data.data)
+      
+//     })
+//     setIdk(iSuckAtThisShit)
+//   }
+//   fetchData()
+// console.log(idk)
 
-            setPlaylistSongs(resolvedSongData);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-
-      fetchData();
-    }, [])
-
-
-if(data){
-  console.log(playlistSongs)
-}
   return (
     <>
-      {/* {data && data !== null ? (
-        playlistSongs.slice(0,5).map(async( song: any, i:number) => {
+      {data && data !== null ? (
+        idk.slice(0,5).map(( song: any, i:number) => {
             
     
           return (
-            <div>lol</div>
-          )
-        })
-      ) : null} */}
-        {/* <div className='playlist-track flex'>
-        {false ? <img src={`${artwork}`} alt="playlist cover" className='empty-div bg-red-700'></img> : <span className='empty-div bg-white-700'></span>}
+            <div className='playlist-track flex'>
+        {false ? <img src={`song.asyncData.data.artwork[]`} alt="playlist cover" className='empty-div bg-red-700'></img> : <span className='empty-div bg-white-700'></span>}
             <div className='flex justify-between playlist-song w-full'>
-                <div className='number '>{i + 1} <span>{name}<span> by {creatorName}</span></span></div>
+                <div className='number '>{i + 1} <span>{song.asyncData.data.title}<span> by {song.asyncData.data.user.name}</span></span></div>
                 <div className='playlist-track-time mr-3 '>4:23 play tiem</div>
             </div>
         </div>
+          )
+        })
+      ) : null}
+      
 
-        <Link to='/moretracks' className='more-tracks p-2'>4 More Tracks</Link> */}
+        
     </>
   )
 }
