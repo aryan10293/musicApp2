d4d875f00c9fdc61545ab38a528c8fa61d907522
aondrej ryan <aondrejryan11@gmail.com>
2023-09-01 10:13:06 -0700

    working on artist page finsihed tracks page

--- client/src/component/Artist.tsx
+++ client/src/component/Artist.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { AsideLeft } from './AsideLeft'
 import { useParams } from 'react-router-dom';
-import { FaMusic } from 'react-icons/fa';
+import { FaMusic, FaRetweet } from 'react-icons/fa';
+import { MdAlbum } from 'react-icons/md';
+import {  faRetweet } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
+import Track from './Track';
 function Artist() {
   const  id  = useParams().id;
   const [userData, setUserData] = React.useState<any>({})
-
+  const [likes, setLikes] = React.useState<string[]>([])
+  const [usertTracks, setUserTracks] = React.useState<any>([])
   React.useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(`https://audius-metadata-2.figment.io/v1/users/${id}?app_name=EXAMPLEAPP`, {
@@ -15,6 +20,20 @@ function Artist() {
       if(response.ok){
         const data = await response.json()
         setUserData(data.data)
+      }
+    }
+    fetchData()
+  }, [])
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://audius-metadata-2.figment.io/v1/users/${id}/tracks?app_name=EXAMPLEAPP`, {
+        method: 'GET'
+      })
+
+      if(response.ok){
+        const data = await response.json()
+        setUserTracks(data.data)
       }
     }
     fetchData()
@@ -29,7 +48,7 @@ function Artist() {
   return (
     <div className='flex'>
        <AsideLeft />
-       <div>
+       <div className='max-h-[100vh] overflow-y-auto'>
         <div className='artist-img' style={containerStyle}>
             <div>
               <p>Artist</p>
@@ -56,20 +75,81 @@ function Artist() {
         </div>
         <div className="user-music-data">
           <ul>
-            <li>
-              <h3><span><FaMusic size={10} color={'blue'}/></span> tracks</h3> 
-            </li>
-            <li>
-              tracks
-            </li>
-            <li>
-              tracks
-            </li>
-            <li>
-              tracks
-            </li>
+            <Link to ='tracks'>
+              <li>
+                <div><FaMusic size={20} color={'blue'}/></div>
+                <span>Tracks</span>
+              </li>
+            </Link>
+            <Link to='albums'>
+              <li>
+                <div><MdAlbum size={20} color={'blue'}/></div>
+                <span>Albums</span>
+              </li>
+            </Link>
+            <Link to='playlist'>
+              <li>
+                <div><FaMusic size={20} color={'blue'}/></div>
+                <span>Playlist</span>
+              </li>
+            </Link>
+            <Link to='repost'>
+              <li>
+                <div><FaRetweet size={20} color={'blue'}/></div>
+                <span>Repost</span>
+              </li>
+            </Link>
           </ul>
         </div>
+        <main className='artist-content'>
+              {usertTracks && usertTracks !== null ? (
+                    // keep trying to get the time from each playlist
+                    usertTracks.map((song:any, i:number): JSX.Element => {
+                        let repost;
+                        let favorites;
+                        let plays;
+                        const seconds = song.duration;
+                        const minutes = Math.floor(seconds / 60);
+                        const remainingSeconds = seconds % 60;
+
+                        const formattedTime = `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+                        if(i === 5){
+                            console.log(song)
+                        }
+                        if(song['repost_count'] <= 999){
+                            repost = song['repost_count']
+                        } else{
+                            repost = `${(song['repost_count'] / 1000).toFixed(2)}K`
+                        }
+
+                        if(song['favorite_count'] <= 999){
+                            favorites = song['favorite_count']
+                        } else{
+                            favorites = `${(song['favorite_count'] / 1000).toFixed(1)}K`
+                        }  
+
+                        if(song['play'] <= 999){
+                            plays = song['play_count']
+                        } else{
+                            plays = `${(song['play_count'] / 1000).toFixed(1)}K`
+                        }
+                        return <Track
+                            likes={likes}
+                            id={song.id}
+                            number={i + 1}
+                            crown={false}
+                            artwork={song.artwork["150x150"]}
+                            timeOfSong={formattedTime}
+                            artistofsong={song.user.name}
+                            artistLink={song.permalink}
+                            repostCount={repost}
+                            favoriteCount={favorites}
+                            plays={plays}
+                            artistId = {song.user.id}
+                            title={song.playlist_name} />;
+                    })
+                ) : <div>loading...</div>}
+        </main>
        </div>
     </div>
   )
